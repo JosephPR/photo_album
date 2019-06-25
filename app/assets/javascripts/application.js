31ec905621c63ffd4fa7a51170cfe0846b6985fc
JosephPR <reilly4181@gmail.com>
2019-06-25 09:18:44 -0600

    styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,19 @@
 //= require popper
 //= require bootstrap
 //= require_tree .
+// 
+// const pokiUrl = 'https://pokeapi.co/api/v2/pokemon'
+// const swapiUrl = 'https://swapi.co/api/people'
+// const marketUrl = 'https://api.worldtradingdata.com/api/v1/stock?symbol=AAPL,PYPL,TWLO,MSFT,FB,.L&api_token=acCz10cbIPNeDpHtlNLT5jsG3J0ivtvcBeQmoOp1GoCFCNkkl9GfsXsT0Nds'
+// // const potterUrl = 'https://www.potterapi.com/v1/characters/key=$2a$10$OVFjNbj9fam8PkHuuQbiOOMsYh/iYhoCr1jwNV5fnb.Q7.ynoPAUW'//$2a$10$OVFjNbj9fam8PkHuuQbiOOMsYh/iYhoCr1jwNV5fnb.Q7.ynoPAUW
+// const potterUrl = 'http://hp-api.herokuapp.com/api/characters'
+//
+// function get() {
+//   return fetch(potterUrl)
+//   .then(response => response.json())
+//   .then(response => {
+//     console.log(response)
+//     return response
+//   })
+// }
+// get()
